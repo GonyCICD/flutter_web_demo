1db879b80212fee3c1a148e9d6d33f9dd60df983
Vuong.VuV <>
2026-08-03 14:43:53 +0700

    show build number

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const buildVersion =
+    String.fromEnvironment('BUILD_NUMBER', defaultValue: '0');
     return MaterialApp(
-      title: 'Flutter Demo Ver 4',
+      title: 'Flutter Demo Ver $buildVersion',
       theme: ThemeData(
         // This is the theme of your application.
         //

00f70998fe80ec4c4704ac5df8d06da09afbf9fe
Vuong.VuV <>
2026-08-03 14:02:48 +0700

    v3

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Ver 2',
+      title: 'Flutter Demo Ver 3',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo  Ver 2 Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo  Ver 3 Home Page'),
     );
   }
 }
